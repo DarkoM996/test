d509b927b6bc055f812d945a5fee9e238e255725
DarkoM996 <darko.mihajlovski96@gmail.com>
2023-05-23 14:54:33 +0200

    new changes

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('clickButton').addEventListener('click', () => {
+    console.log('click')
+    document.getElementById('clickOutput').style.backgroundColor='blue'
+})
+// Get DocumentbyId (ime na button, add eventlistener e tip na event)
+document.getElementById('doubleClick').addEventListener('click', () => {
+    console.log('click');
+    //getelementbyid e ime na div
+    document.getElementById('dbloutput').style.backgroundColor='green'
+})
+
+
